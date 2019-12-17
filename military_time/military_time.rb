4112b7239943158d4bf45a0b1fcbfcab17669b56
Jem Pimentel <jem.pimentel@outlook.com>
2019-12-17 13:13:39 +0800

    Runs linter

--- military_time/military_time.rb
+++ military_time/military_time.rb
@@ -1,16 +1,17 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-print "Enter time (HHMM):"
+print 'Enter time (HHMM):'
 military_time = gets.chomp
 begin
-military_time =  Integer (military_time) 
-if (military_time > 2400) || (military_time % 100 > 59)
-    puts "Invalid time"      
-elsif (military_time < 1259) 
-    puts "\n non-Military Time:" + military_time.to_s + "AM"
-else
-    print "\n non-Military Time:" + (military_time - 1200).to_s + "PM"
-end
-rescue 
-    puts "Invalid Input!"
+  military_time = Integer military_time
+  if (military_time > 2400) || (military_time % 100 > 59)
+    puts 'Invalid time'
+  elsif military_time < 1259
+    puts "\n non-Military Time:" + military_time.to_s + 'AM'
+  else
+    print "\n non-Military Time:" + (military_time - 1200).to_s + 'PM'
+  end
+rescue
+  puts 'Invalid Input!'
 end
