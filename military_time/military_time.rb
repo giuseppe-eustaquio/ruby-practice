c12eab4b6327028e14fd49b5da88fe6a4f1606eb
Giuseppe Eustaquio <gseppe222@gmail.com>
2019-12-19 12:15:55 +0800

    Fixed multiple logical errors: time formatting, conversion of integers beginning with '0' to octal, fixed 12:xx AM format

--- military_time/military_time.rb
+++ military_time/military_time.rb
@@ -4,13 +4,22 @@
 print 'Enter time (HHMM):'
 military_time = gets.chomp
 begin
-  military_time = Integer military_time
-  if (military_time > 2400) || (military_time % 100 > 59)
+  military_time = Integer (military_time)
+  mm = (military_time % 100)
+  string_mm = "%02d" % mm
+  if military_time < 1200
+    meridies = "AM"
+  else
+    meridies = "PM"
+  end
+  if (military_time > 2400) || (mm > 59)
     puts 'Invalid time'
   elsif military_time < 1259
-    puts "\n non-Military Time:" + military_time.to_s + 'AM'
+    hh = (military_time - mm) / 100
+    puts "\n non-Military Time: #{hh}:#{string_mm} #{meridies}"
   else
-    print "\n non-Military Time:" + (military_time - 1200).to_s + 'PM'
+    hh = ((military_time - mm - 1200) % 1000) / 100
+    print "\n non-Military Time: #{hh}:#{string_mm} #{meridies}"
   end
 rescue
   puts 'Invalid input!'
