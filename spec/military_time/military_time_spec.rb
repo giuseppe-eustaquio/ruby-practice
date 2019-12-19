8dc821745add867505d609d2ce9da6557db47b17
Jem Pimentel <jem.pimentel@outlook.com>
2019-12-20 00:05:36 +0800

    Improves tests

--- spec/military_time/military_time_spec.rb
+++ spec/military_time/military_time_spec.rb
@@ -11,38 +11,43 @@ describe MilitaryTime do
       let(:time_starting_with_0) { 130 }
       let(:midnight) { 0 }
       let(:noon) { 1200 }
+      let(:time_with_decimal_places) { 1330.99 }
 
-      it "creates a valid time am time" do
+      it "connverts to the correct am time" do
         expect(mil_time.new(am_time).call).to eql("10:30 AM")
       end
 
-      it "creates a valid time pm time" do
+      it "converts to the correct pm time" do
         expect(mil_time.new(pm_time).call).to eql("01:30 PM")
       end
 
-      it "correctly handles military times that start with 0" do
+      it "converts military times that start with 0" do
         expect(mil_time.new(time_starting_with_0).call).to eql("01:30 AM")
       end
 
-      it "correctly handles noon" do
+      it "handles noon" do
         expect(mil_time.new(noon).call).to eql("12:00 PM")
       end
 
-      it "correctly handles midnight" do
+      it "handles midnight" do
         expect(mil_time.new(midnight).call).to eql("12:00 AM")
+      end
+
+      it "ignores any decimal places and still converts" do
+        expect(mil_time.new(time_with_decimal_places).call).to eql("01:30 PM")
       end
     end
 
-    context "when passing an invalid time" do
+    context "when passing an invalid time in military format" do
       let(:negative_time) { -1030 }
       let(:overflow_time) { 2400 }
-      let(:invalid_minutes) { 190 }
+      let(:invalid_minutes) { 199 }
 
       it "throws an exception when time is negative" do
         expect { mil_time.new(negative_time).call }.to raise_error(ArgumentError)
       end
 
-      it "throws an exception when time is too big" do
+      it "throws an exception when time is greater than 2359" do
         expect { mil_time.new(overflow_time).call }.to raise_error(ArgumentError)
       end
 
