db9d5b9f30a9f6ae02aa5f548e27af60856acac2
Jem Pimentel <jem.pimentel@outlook.com>
2019-12-19 19:37:31 +0800

    Changes wording

--- military_time/miltime_convert.rb
+++ military_time/miltime_convert.rb
@@ -4,7 +4,10 @@ require_relative "military_time"
 
 print "Input military time (xxxx): "
 
-# Get a number from stdin. Make it base-10 to avoid converting 0123 to octal
-military_time = Integer(gets, 10)
-
-puts MilitaryTime.new(military_time).call
+begin
+  # Get a number from stdin. Make it base-10 to avoid converting "0123" to octal
+  military_time = Integer(gets, 10)
+  puts MilitaryTime.new(military_time).call
+rescue ArgumentError
+  puts "You've entered an invalid time."
+end
