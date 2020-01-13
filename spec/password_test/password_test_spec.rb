3780a4f25ed9fa56896594480360452f566592c3
Giuseppe Eustaquio <gseppe222@gmail.com>
2020-01-13 17:37:16 +0800

    Adds edge cases and fixes indentation/formatting

--- spec/password_test/password_test_spec.rb
+++ spec/password_test/password_test_spec.rb
@@ -1,36 +1,54 @@
 require_relative "../../password-strength/password_validator"
 
 describe PasswordValidator do
-  describe "#password_strength" do
-    subject(:pw_vil) { described_class }
+  describe ".password_strength" do
+    subject(:pw_val) { described_class }
 
     context "when entering a password" do
       let(:very_strong_password) { "G!aa1dsa" }
-      let(:strong_password) { "A1aaaaaaa"}
-      let(:weak_password) { "aaaawwww"}
-      let(:very_weak) { "1"}      
-      let(:empty_password) { ""}
-      let(:single_space) { " "}
+      let(:strong_password) { "A1aaaaaaa" }
+      let(:weak_password) { "aaaawwww" }
+      let(:very_weak) { "1" }      
+      let(:empty_password) { "" }
+      let(:single_space) { " " }
+      let(:long_string_letters) { "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" }
+      let(:long_string_numbers) { "11111111111111111111111111111111111111111111111111111111111111111111111111111111111" }
+      let(:long_string_spec_char) { "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" }
+
 
       it "tests very strong password" do
-        expect(pw_vil.password_strength(very_strong_password)).to eql("very strong")
+        expect(pw_val.password_strength(very_strong_password)).to eql("very strong")
       end
 
       it "tests strong password" do
-        expect(pw_vil.password_strength(strong_password)).to eql("strong")
+        expect(pw_val.password_strength(strong_password)).to eql("strong")
       end
       it "tests weak password" do
-        expect(pw_vil.password_strength(weak_password)).to eql("weak")
+        expect(pw_val.password_strength(weak_password)).to eql("weak")
       end
       it "tests very weak password" do
-        expect(pw_vil.password_strength(very_weak)).to eql("very weak")
+        expect(pw_val.password_strength(very_weak)).to eql("very weak")
       end
       it "tests empty password" do
-        expect(pw_vil.password_strength(empty_password)).to eql("very weak")
+        expect(pw_val.password_strength(empty_password)).to eql("very weak")
       end
       it "tests single space password" do
-        expect(pw_vil.password_strength(single_space)).to eql("very weak")
+        expect(pw_val.password_strength(single_space)).to eql("very weak")
       end
+      it "tests a long string of letters" do
+        expect(pw_val.password_strength(long_string_letters)).to eql("weak")
+      end
+      it "tests a long string of numbers" do
+        expect(pw_val.password_strength(long_string_numbers)).to eql("very weak")
+      end
+      it "tests a long string of numbers" do
+        expect(pw_val.password_strength(long_string_numbers)).to eql("very weak")
+      end
+      it "tests a long string of string" do
+        expect(pw_val.password_strength(long_string_spec_char)).to eql("very weak")
+      end
+     
+
    end
   end
 end
