b1fea1bbdeafd497a65bf95aa01644dd4c0d2954
Giuseppe Eustaquio <gseppe222@gmail.com>
2020-01-30 17:37:53 +0800

    Fixes schema table (remove posts table)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :email, presence: true
   validates :name, length: { within: 1..20 }
+  has_many :microposts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_24_072828) do
+ActiveRecord::Schema.define(version: 2020_01_30_093301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
+  create_table "microposts", force: :cascade do |t|
+    t.string "title"
+    t.string "text"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,4 +31,5 @@ ActiveRecord::Schema.define(version: 2020_01_24_072828) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "microposts", "users"
 end
